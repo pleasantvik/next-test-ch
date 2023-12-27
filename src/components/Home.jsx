3405e342f70c331462721078281d7942a40e9203
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-12-27 14:54:50 +0100

    input style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   const navigate = useNavigate();
   return (
     <section className="container mx-auto grid grid-cols-1 md:grid-cols-2">
-      <div className=" ml-[100px] bg-primary h-[812px] md:flex md:flex-col pl-8 pt-44 gap-4 hidden home relative">
+      <div className="ml-[100px] bg-primary h-[812px] md:flex md:flex-col pl-8 pt-44 gap-4 hidden home relative">
         <div>
           <img src={logo} alt="Logo" />
         </div>
@@ -21,32 +21,41 @@ const Home = () => {
           <img src={house} alt="house" className="w-full object-cover" />
         </div>
       </div>
-      <div className="h-[950px] flex flex-col pt-20 md:pt-64 text-center pl-10 w-[80%] mx-auto ">
-        <div className="flex justify-center md:hidden">
-          <img src={logo} alt="Logo" />
+      <div className="h-[950px] flex flex-col pt-20 md:pt-64 text-center  w-[80%] mx-auto ">
+        <div className="flex justify-center md:hidden mb-10">
+          <img src={logo} alt="Logo" className="w-[20%]" />
         </div>
 
-        <h2 className="md:hidden">Let's get Started</h2>
+        <h2 className="text-2xl font-semibold md:hidden">Let's get Started</h2>
         <h2 className="hidden md:block">Provide your phone number</h2>
-        <p className="mb-8">
-          <small>
+        <p className="mb-8 px-14">
+          <small className="text-lg text-[#585858] poppins">
             Kindly use an active WhatsApp number, and we'll send a confirmation
-            message to <br /> the provided number.
+            message to the provided number.
           </small>
         </p>
 
-        <div className="w-full flex justify-center gap-2">
-          <div className="border-2 border-dark">+356</div>
+        <div className="w-full flex items-center justify-center gap-2">
+          <select name="" className="text-center rounded-md " id="">
+            <option
+              className="flex items-center justify-center px-4"
+              value="+234"
+            >
+              +234
+            </option>
+          </select>
           <input
             type="text"
-            className="border-2 border-dark rounded-lg w-4/6"
+            // className="border-2 border-dark rounded-lg w-4/6"
+            // className=" w-full placeholder:text-slate-400 h-[54px]  rounded-md border-2 focus:border-2 bg-transparent px-4 text-center focus:!border-primary focus:!outline-primary focus:!ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+            className="w-4/6 rounded-md input"
             placeholder="Phone Number"
           />
         </div>
         <div className="mt-10 mb-4">
           <button
             onClick={() => navigate("/main")}
-            className="w-[95%] mx-auto flex justify-center bg-[#34333e] py-4 text-white"
+            className="w-[80%] mx-auto flex justify-center bg-[#34333e] py-4 text-white"
           >
             Continue
           </button>
@@ -54,7 +63,7 @@ const Home = () => {
         <div className="">
           <button
             onClick={() => navigate("/main")}
-            className="w-[95%] mx-auto flex justify-center text-[#34333e] py-4 border-2 border-dark-main"
+            className="w-[80%] mx-auto flex justify-center text-[#34333e] py-4 border-2 border-dark-main"
           >
             Have an Account? Login
           </button>
